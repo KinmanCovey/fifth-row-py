796187eec29864d94d39325c43800173e8690d2f
Kinman Covey <kinmancovey@yahoo.com>
2015-04-22 12:46:59 -0700

    added test file, updated travis.yml

---
+++ fifth-row-test.py
@@ -0,0 +1,27 @@
+import unittest
+from fifth_row import FifthRow, Matchup, TokenError
+
+class FifthRowTest(unittest.TestCase):
+
+    def test_url(self):
+        '''
+        FifthRow object's url should be the sandboxed url
+        '''
+        self.assertEqual(FifthRow(sandbox=True).url, 'sandbox.the5throw.com')
+
+    def test_get_return(self):
+        '''
+        FifthRow.get() should return a list of Matchup objects
+        '''
+        data = FifthRow(sandbox=True).get('nba')
+        self.assertIs(type(data), list)
+        self.assertEqual(data[0].__class__, Matchup)
+
+    def test_token_error(self):
+        '''
+        TokenError should be raised if token != an integer
+        '''
+        self.assertRaises(TokenError, FifthRow, sandbox=False, token=None)
+
+if __name__ == '__main__':
+    unittest.main()
